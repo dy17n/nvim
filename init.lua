3b6d3197a4f886d70d5bb449944c49267a07f7d9
dylan <krecker@keemail.me>
2025-01-23 19:01:30 -0600

    new theme

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 require("pam").manage({
   { source = "mvllow/pam.nvim" },
-  { source = "Shatur/neovim-ayu" },
   { source = "neovim/nvim-lspconfig" },
+	{ source = "rose-pine/neovim", as = "rose-pine" },
 	{ source = "nvim-treesitter/nvim-treesitter" }
 })
 
-vim.cmd 'colorscheme ayu-dark'
+vim.cmd 'colorscheme rose-pine'
 vim.cmd 'set shiftwidth=2'
 vim.cmd 'set tabstop=2'
 vim.cmd 'set number'
