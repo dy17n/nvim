8f24bfe8fd8d5dfee173b87d37aabd4a85a10c95
Dylan <krecker@keemail.me>
2025-02-09 12:55:50 -0600

    moon

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
-require("pam").manage({
+﻿require("pam").manage({
   { source = "mvllow/pam.nvim" },
   { source = "neovim/nvim-lspconfig" },
 	{ source = "rose-pine/neovim", as = "rose-pine" },
+	{ source = "pcostasgr/red_alert_vim_theme"},
 	{ source = "nvim-treesitter/nvim-treesitter" }
 })
 
-vim.cmd 'colorscheme rose-pine'
+vim.cmd 'colorscheme rose-pine-moon'
 vim.cmd 'set shiftwidth=2'
 vim.cmd 'set tabstop=2'
 vim.cmd 'set number'
