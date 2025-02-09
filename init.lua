d7c767e3dc6bba7ff1a121547d1c387384996bef
Dylan <krecker@keemail.me>
2025-02-09 12:20:38 -0600

    treesitter

--- init.lua
+++ init.lua
@@ -9,9 +9,18 @@ vim.cmd 'colorscheme rose-pine'
 vim.cmd 'set shiftwidth=2'
 vim.cmd 'set tabstop=2'
 vim.cmd 'set number'
+vim.cmd 'set relativenumber'
 vim.cmd 'set cursorline'
 vim.cmd 'set signcolumn=yes'
 vim.opt.fillchars = { eob = " "}
 vim.diagnostic.config({ virtual_text = false })
 
-require'lspconfig'.hls.setup{}
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { all },
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true
+	}
+}
